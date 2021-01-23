59d6ba92dbfe8eb254461dd12dfd5440584aa301
Rachid ETTABAAI <rachid.ettabaai.simplon@gmail.com>
2021-01-23 15:50:09 +0100

    modify blade template name for home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,7 +19,7 @@
                 <select class="custom-select" id="select-country">
                     <option value="none" selected>Choose...</option>
                     @foreach($countries as $country)
-                        <option value="" >{{ $country["name"] }}</option>
+                        <option value="{{ $country['alpha2Code'] }}" >{{ $country["name"] }}</option>
                     @endforeach
                 </select>
             </div>
