fd139942a348bdb6a414c5f3e24c44afc439a387
Rachid ETTABAAI <rachid.ettabaai.simplon@gmail.com>
2021-01-25 02:29:05 +0100

    add datatable and blade template for data page

--- resources/views/data.blade.php
+++ resources/views/data.blade.php
@@ -13,7 +13,7 @@
 @section("content")
 <main role="main" class="col-md-9 ml-sm-auto col-lg-10 mt-4">
 
-    {{--  @if($errorsresults != "")
+    @if($errorsresults != "")
 
         <div class="container text-center mb-2">
             <div class="row mb-2">
@@ -27,7 +27,7 @@
 
     @endif
 
-    @if($errorsresults == "")  --}}
+    @if($errorsresults == "")
 
     <div class="container text-center mb-2">
         <div class="row mb-2">
@@ -163,8 +163,10 @@
             </div>
         
         </div>
+    
+    </div>
 
-    {{--  @endif  --}}
+    @endif
         
 </main>
 
