92d9235fe57ce6c7248e6727ae8efdcbc3592aea
Rachid ETTABAAI <rachid.ettabaai.simplon@gmail.com>
2021-01-25 02:40:08 +0100

    add datatable and blade template for data page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" >
     <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
-    <meta name="csrf-token" content="{{ csrf_token() }}" />
+    @if (Request::path() == "/")
+	    <meta name="csrf-token" content="{{ csrf_token() }}" />
+    @endif
     @yield('stylesheet')
   </head>
   <body>
