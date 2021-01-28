cf6068fd32e56b91981802431f8d6d46d7686aac
Rachid ETTABAAI <rachid.ettabaai.simplon@gmail.com>
2021-01-28 02:10:23 +0100

    add jvectormap and blade template for map page

--- resources/js/mappage/map.js
+++ resources/js/mappage/map.js
@@ -2,6 +2,7 @@ let $ = require("jquery");
 
 require('jvectormap-next')($);
 $.fn.vectorMap('addMap', 'world_mill', require('jvectormap-content/world-mill'));
+$.noConflict();
 
 $(document).ready(function() {
 
