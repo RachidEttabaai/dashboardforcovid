ce0229a04e49d2d265220f911e0089a4e867040b
Rachid ETTABAAI <rachid.ettabaai.simplon@gmail.com>
2021-02-07 23:56:27 +0100

    add about page and sort select option for countries

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,9 +9,8 @@ class HomeController extends Controller
     public function show()
     {
         $countries = (new Apicalling("https://api.covid19api.com/countries"))->apirequest();
-        $iso2 = array_column($countries,"ISO2");
         $country = array_column($countries,"Country");
-        array_multisort($iso2,SORT_ASC,$country,SORT_ASC,$countries);
+        array_multisort($country,SORT_ASC,$countries);
         //dd($countries);
         return view("home",compact("countries"));
     }
