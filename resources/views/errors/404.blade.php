201fdfef854e1088bbd6a2760c67fcf9883e2547
Rachid ETTABAAI <rachid.ettabaai.simplon@gmail.com>
2021-02-08 01:19:36 +0100

    add custom error pages

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -5,7 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
-    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" >
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@600;900&display=swap" rel="stylesheet">
   </head>
   <body>
 
